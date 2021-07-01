600fa3f8b0a8a978814f97a237de8b625903d0ff
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-07-01 13:03:36 +0900

    20210701

--- 1759.cpp
+++ 1759.cpp
@@ -7,12 +7,10 @@
 using namespace std;
 
 int l, c;
-vector<char> v1;
-vector<char> v2;
 char arr[17];
 vector<char> selected;
 bool visit[17];
-void solution(int idx , int cnt, int max);
+void solution(int idx, int cnt, int max);
 int main() {
 	cin >> l >> c;
 
@@ -21,21 +19,21 @@ int main() {
 		cin >> arr[i];
 	}
 	sort(arr, arr + c);
-	solution(0,0, l);
+	solution(0, 0, l);
 }
 
-void solution(int idx , int cnt, int max)
+void solution(int idx, int cnt, int max)
 {
 	if (cnt == max)
 	{
-	
+
 		//순서 체크
 		for (int i = 0; i < max - 1; i++)
 		{
 			if (selected[i] > selected[i + 1])
 				return;
 		}
-		int chk1=0, chk2=0;
+		int chk1 = 0, chk2 = 0;
 		for (int i = 0; i < max; i++)
 		{
 			if (selected[i] == 'a' || selected[i] == 'e' || selected[i] == 'i' || selected[i] == 'o' || selected[i] == 'u')
@@ -60,7 +58,7 @@ void solution(int idx , int cnt, int max)
 		{
 			visit[i] = 1;
 			selected.push_back(arr[i]);
-			solution(i+ 1, cnt + 1, max);
+			solution(i + 1, cnt + 1, max);
 			visit[i] = 0;
 			selected.pop_back();
 		}
